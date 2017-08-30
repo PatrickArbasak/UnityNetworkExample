b3a42afe2639ad5f66426c3364d589b202d560e2
Patrick Arbasak <pat.arbasak@gmail.com>
2017-08-29 17:54:40 -0700

    Initial Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health health = null;
+        if (health = collision.gameObject.GetComponent<Health>())
+        {
+            health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class PlayerController : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // Only the local players processe input.
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            CmdFire();
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    public void CmdFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
+}
